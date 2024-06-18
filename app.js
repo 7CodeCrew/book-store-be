426ce1d59a24f19d2a9b04685d864173ca521fc3
hunjoonrhee <hunjoonrhee90@gmail.com>
2024-06-18 23:58:17 +0200

    도서 불러오기 (초기화), 환경변수 디폴트

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const axios = require('axios');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const session = require('express-session');
+const Book = require('./models/book');
+const fetchNewBooks = require('./scripts/fetchNewBooks');
 
 const app = express();
 
@@ -27,26 +30,29 @@ dotenv.config();
 //   }),
 // );
 
-// const MONGODB_URI = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI_PROD : process.env.MONGODB_URI_DEV;
-// console.log(MONGODB_URI);
-// mongoose
-//   .connect(MONGODB_URI)
-//   .then(() => {
-//     console.log('mongoose connected');
-//   })
-//   .catch((err) => {
-//     console.log('mongoose connection failed', err);
-//   });
+// 몽고 디비 URI: 프로덕션 환경일 경우 MONGODB_URI_PROD (몽고 아틀라스), 아니면 로컬호스트.
+const MONGODB_URI = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI_PROD : process.env.MONGODB_URI_DEV;
+console.log(MONGODB_URI);
+mongoose
+  .connect(MONGODB_URI)
+  .then(async () => {
+    console.log('mongoose connected');
 
-// app.get('/', (req, res) => {
-//   res.send('Hello Joon');
-// });
-
-// app.use('/api/user', userRouter);
-// app.use('/api/product', productRouter);
-// app.use('/api/cart', cartRouter);
-// app.use('/api/order', orderRouter);
-
-app.listen(process.env.PORT || 5002, () => {
-  console.log('server started and listening port 5002');
-});
+    // 데이터베이스에 books Document가 있는 지 확인하고 
+    const bookCount = await Book.countDocuments();
+    if (bookCount === 0) {
+      // 없으면 도서를 불러온다
+      console.log('No books found in database. Fetching books from external API.');
+      await fetchNewBooks();
+    } else {
+      // 있으면 도서를 불러오지 않는다.
+      console.log('Books already exist in the database. Skipping fetch.');
+    }
+    // 서버 시작
+    app.listen(process.env.PORT || 4000, () => {
+      console.log(`Server is running on port 4000`);
+    });
+  })
+  .catch((err) => {
+    console.log('mongoose connection failed', err);
+  });
