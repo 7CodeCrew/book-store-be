0f3cdfcfc503dae7a51e76423aa6f1ef97090cc0
hunjoonrhee <hunjoonrhee90@gmail.com>
2024-06-18 06:47:26 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const session = require('express-session');
+
+const app = express();
+
+dotenv.config();
+
+// app.use(bodyParser.json());
+
+// app.use(
+//   cors({
+//     origin: ['http://localhost:3000'],
+//     credentials: true,
+//   }),
+// );
+
+// app.use(
+//   session({
+//     saveUninitialized: false,
+//     resave: false,
+//     secret: process.env.COOKIE_SECRET,
+//     cookie: { secure: true },
+//   }),
+// );
+
+// const MONGODB_URI = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI_PROD : process.env.MONGODB_URI_DEV;
+// console.log(MONGODB_URI);
+// mongoose
+//   .connect(MONGODB_URI)
+//   .then(() => {
+//     console.log('mongoose connected');
+//   })
+//   .catch((err) => {
+//     console.log('mongoose connection failed', err);
+//   });
+
+// app.get('/', (req, res) => {
+//   res.send('Hello Joon');
+// });
+
+// app.use('/api/user', userRouter);
+// app.use('/api/product', productRouter);
+// app.use('/api/cart', cartRouter);
+// app.use('/api/order', orderRouter);
+
+app.listen(process.env.PORT || 5002, () => {
+  console.log('server started and listening port 5002');
+});
