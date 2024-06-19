2c09ca23f8119fccf325d5966e5a2e29fd8232d1
MayHyeyeonKim <hkim7963@gmail.com>
2024-06-19 02:19:57 -0700

    feat: login & register (requires JWT_SECRET_KEY in environment variables)

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ const dotenv = require('dotenv');
 const session = require('express-session');
 const Book = require('./models/book');
 const fetchNewBooks = require('./scripts/fetchNewBooks');
-
+const indexRouter = require('./routes/index');
 const app = express();
 
 dotenv.config();
-
-// app.use(bodyParser.json());
-
+app.use(cors()); //개발환경에서 모든 오리진 허용으로 사용중
 // app.use(
 //   cors({
 //     origin: ['http://localhost:3000'],
 //     credentials: true,
 //   }),
 // );
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // app.use(
 //   session({
@@ -29,6 +29,7 @@ dotenv.config();
 //     cookie: { secure: true },
 //   }),
 // );
+app.use('/api/', indexRouter);
 
 // 몽고 디비 URI: 프로덕션 환경일 경우 MONGODB_URI_PROD (몽고 아틀라스), 아니면 로컬호스트.
 const MONGODB_URI = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI_PROD : process.env.MONGODB_URI_DEV;
@@ -38,7 +39,7 @@ mongoose
   .then(async () => {
     console.log('mongoose connected');
 
-    // 데이터베이스에 books Document가 있는 지 확인하고 
+    // 데이터베이스에 books Document가 있는 지 확인하고
     const bookCount = await Book.countDocuments();
     if (bookCount === 0) {
       // 없으면 도서를 불러온다
