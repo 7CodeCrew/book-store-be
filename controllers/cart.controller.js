ba10e18d4188abc3584bb87e1dfb380d0b2da418
문효안 <hyoanmoon@munhyoan-ui-MacBookAir.local>
2024-06-23 17:47:50 +0900

    cartList error

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,11 +1,11 @@
+const Book = require('../models/Book');
 const Cart = require('../models/Cart');
+const User = require('../models/User');
 const cartController = {};
 
 cartController.addItemToCart = async (req, res) => {
   try {
     // 1. 정보 가져오기
-    // 1-1. User Id
-    // 1-2. 상품 Id, qty
     const { userId } = req;
     const { bookId, qty } = req.body;
     // 1.5 유저를 가지고 카트 찾기 카트가 없으면 새로 만들어 주기
@@ -21,11 +21,42 @@ cartController.addItemToCart = async (req, res) => {
     if (existItem) {
       throw new Error('Item alreay exist.');
     }
-
     // 2. 새로운 아이템을 카트에 추가
     cart.items = [...cart.items, { bookId, qty }];
     await cart.save();
     res.status(200).json({ status: 'Success', data: cart, cartItemQty: cart.items.length });
+  } catch (error) {
+    return res.status(400).json({ status: 'Fail..', message: error.message });
+  }
+};
+
+// cartController.getCartList = async (req, res) => {
+//   try {
+//     const { userId } = req;
+//     const cartList = await Cart.findOne({ userId }).populate({
+//       path: 'items',
+//       populate: {
+//         path: 'bookId',
+//         model: Book,
+//       },
+//     });
+//     res.status(200).json({ status: 'Success', data: cartList.items });
+//   } catch (error) {
+//     return res.status(400).json({ status: 'Fail..', message: error.message });
+//   }
+// };
+
+cartController.getCartList = async (req, res) => {
+  try {
+    const { userId } = req;
+    const cartList = await Cart.findOne({ userId }).populate({
+      path: 'items',
+      populate: {
+        path: 'bookId',
+        model: Book,
+      },
+    });
+    res.status(200).json({ status: 'Success', data: cartList.items });
   } catch (error) {
     return res.status(400).json({ status: 'Fail..', message: error.message });
   }
