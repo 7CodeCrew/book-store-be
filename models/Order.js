5314571f5b42a4745cdaf86e477be10ec796df4a
문효안 <hyoanmoon@munhyoan-ui-MacBookAir.local>
2024-06-25 00:13:52 +0900

    근선님의Order 스키마 업뎃

--- models/Order.js
+++ models/Order.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
-
-const orderSchema = new mongoose.Schema(
+const Schema = mongoose.Schema;
+const orderSchema = Schema(
   {
-    userID: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    userID: { type: mongoose.ObjectId, ref: 'User' },
     contact: { type: Object, required: true },
     shipTo: { type: Object, required: true },
     totalPrice: { type: Number, default: 0, required: true },
     status: { type: String, default: 'preparing' },
     items: [
       {
-        bookID: { type: mongoose.Schema.Types.ObjectId, ref: 'Book' },
+        bookID: { type: mongoose.ObjectId, ref: 'Book' },
         qty: { type: Number, default: 1, required: true },
         price: { type: Number, required: true },
       },
@@ -18,13 +18,10 @@ const orderSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
-
 orderSchema.methods.toJSON = function () {
   const obj = this._doc;
   delete obj.__v;
   return obj;
 };
-
 const Order = mongoose.model('Order', orderSchema);
-
 module.exports = Order;
