7056d1212fe7a8a47d91bcc48cde2ef6501192fa
geunseonkim <guensoen@gmail.com>
2024-06-25 02:20:30 +1200

    add wishlist feature with favorite button functionality

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,13 @@ const userApi = require('./user.api');
 const bookApi = require('./book.api');
 const categoryApi = require('./category.api');
 const orderApi = require('./order.api');
+const favoriteApi = require('./favorite.api');
 
 Router.use('/auth', authApi);
 Router.use('/user', userApi);
 Router.use('/book', bookApi);
 Router.use('/category', categoryApi);
 Router.use('/order', orderApi);
+Router.use('/favorite', favoriteApi);
 
 module.exports = Router;
